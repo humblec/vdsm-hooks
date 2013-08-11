b224bcfdc5f0c3b94dab9015ef01fdcd56a16b47
Humble Chirammal <hchiramm@redhat.com>
2013-08-11 13:51:24 +0530

    hooks: macbind - This hook support binding specified
    mac address to custom/other bridge than the currently defined
    bridge in ovirt. This hook is also capable of binding
    a mac address to openvswitch bridge.
    
    Signed-off-by: Humble Chirammal <hchiramm@redhat.com>

---
+++ macbind/before_vm_start.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+import os
+import sys
+import hooking
+import traceback
+
+'''
+
+macbind:
+
+syntax:
+macbind=macaddress-brName-portType,...
+refer README for more details.
+
+
+if 'ovs' as portType:
+
+<interface type='bridge'>
+ <mac address='00:1a:4a:41:d2:5f'/>
+ <source bridge='ovsbr0'/>
+ <model type='virtio'/>
+ <virtualport type='openvswitch'/>
+ <filterref filter='vdsm-no-mac-spoofing'/>
+ <link state='up'/>
+ <address type='pci' domain='0x0000' bus='0x00' slot='0x04' function='0x0'/>
+</interface>
+
+If 'lb' or '' as portType:
+The given bridge will be replaced with current bridge:
+
+<interface type='bridge'>
+  <mac address='00:1a:4a:41:d2:b8'/>
+  <source bridge='br0'/>
+  <model type='virtio'/>
+  <filterref filter='vdsm-no-mac-spoofing'/>
+  <link state='up'/>
+  <address type='pci' domain='0x0000' bus='0x00' slot='0x04' function='0x0'/>
+</interface>
+
+'''
+
+
+def createVportElement(domxml, porttype):
+    vPort = domxml.createElement('virtualport')
+    vPort.setAttribute('type', 'openvswitch')
+    return vPort
+
+
+def createSbridgeElement(domxml, brName):
+    sBridge = domxml.createElement('source')
+    sBridge.setAttribute('bridge', brName)
+    return sBridge
+
+
+def removeElement(interface, Element):
+    interface.removeChild(Element)
+
+
+if 'macbind' in os.environ:
+    try:
+
+        macbinds = os.environ['macbind']
+        domxml = hooking.read_domxml()
+        macAddr = ''
+        brName = ''
+        pType = ''
+
+        for nic in macbinds.split(','):
+            try:
+                macAddr, brName, pType = nic.split('-')
+                macAddr = macAddr.strip()
+                brName = brName.strip()
+                pType = pType.strip()
+
+            except ValueError:
+                sys.stderr.write('macbind: input error, expected '
+                                 'macbind:macAddr:brName:pType,'
+                                 'where brName is bridgename'
+                                 'pType can be ovs|lb')
+                sys.exit(2)
+            if pType == "ovs":
+                command = ['/usr/bin/ovs-vsctl', 'br-exists %s' % (brName)]
+                retcode, out, err = hooking.execCmd(
+                    command, sudo=False, raw=True)
+                if retcode != 0:
+                    sys.stderr.write('macbind: Error in finding ovsbridge:'
+                                     '%s: %s, err = %s' %
+                                     (brName, ' '.join(command), err))
+                    continue
+            if pType == "lb" or pType == '':
+                command = ['/usr/sbin/brctl', 'show', brName]
+                retcode, out, err = hooking.execCmd(
+                    command, sudo=False, raw=True)
+
+                if err or retcode != 0:
+                    sys.stderr.write('macbind: Error in finding Linuxbridge:'
+                                     ' %s \n: %s, err = %s\n' %
+                                    (brName, ' '.join(command), err))
+                    continue
+
+            for interface in domxml.getElementsByTagName('interface'):
+                for macaddress in interface.getElementsByTagName('mac'):
+                    addr = macaddress.getAttribute('address')
+                    if addr == macAddr:
+                        for bridge in interface.getElementsByTagName('source'):
+                            interface.removeChild(bridge)
+                            interface.appendChild(
+                                createSbridgeElement(domxml, brName))
+                            if pType == "ovs":
+                                interface.appendChild(
+                                    createVportElement(domxml, 'openvswitch'))
+
+        hooking.write_domxml(domxml)
+    except:
+        sys.stderr.write('macbind: [unexpected error]: %s\n' %
+                         traceback.format_exc())
+        sys.exit(2)
